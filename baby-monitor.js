8a9fb4f9e88c24942cc814b2e4f0c6867b2f8d23
Patrick Klingemann <patrick.klingemann@gmail.com>
2012-04-13 18:41:28 -0400

    when was the last diaper change!?!?!

--- baby-monitor.js
+++ baby-monitor.js
@@ -1,13 +1,24 @@
+DiaperChanges = new Meteor.Collection('diaper_changes');
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to baby-monitor.";
+  Template.diaper_changes.lastDiaperChanged = function () {
+    lastDiaperChange = DiaperChanges.findOne({}, { sort: { created_at: -1 } });
+    if (typeof lastDiaperChange !== 'undefined')
+      return lastDiaperChange.created_at
+    else
+      return new Date();
   };
 
-  Template.hello.events = {
+  Template.diaper_changes.lastDiaperChangedDisplay = function () {
+    return Date.create(Template.diaper_changes.lastDiaperChanged()).format('at {12hr}:{mm}{tt} on {Weekday}');
+  };
+
+  Template.diaper_changes.events = {
     'click input' : function () {
       // template data, if any, is available in 'this'
+      DiaperChanges.insert({ created_at: new Date() });
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log("Diaper change!");
     }
   };
 }
